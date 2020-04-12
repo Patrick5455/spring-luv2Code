cc382a6a90601623e202bd6536b7ace11f3863ae
Patrick5455 <patrickojunde@gmail.com>
2020-04-12 12:37:25 +0100

    Injecting Literal values

--- spring-demo-one/src/com/springdemo/CricketCoach.java
+++ spring-demo-one/src/com/springdemo/CricketCoach.java
@@ -3,6 +3,8 @@ package com.springdemo;
 public class CricketCoach implements Coach  {
 
 	private FortuneService fortuneService;
+	private String email;
+	private String team;
 	
 	//A no-arg constructor is needed for a setter injection
 	
@@ -13,6 +15,24 @@ public class CricketCoach implements Coach  {
 	public void setFortuneService(FortuneService fortune) {
 		System.out.println("Inside the setter injection");
 		fortuneService = fortune;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		System.out.println("SetEmail is secured");
+		this.email = email;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+	public void setTeam(String team) {
+		System.out.println("SetTeam is secured");
+		this.team = team;
 	}
 
 	@Override
