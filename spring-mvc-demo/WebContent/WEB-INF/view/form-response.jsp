0950e75e5e9c83e39925ac11d0992fefe95b4634
Patrick5455 <patrickojunde@gmail.com>
2020-04-14 11:25:12 +0100

    Working with models

--- spring-mvc-demo/WebContent/WEB-INF/view/form-response.jsp
+++ spring-mvc-demo/WebContent/WEB-INF/view/form-response.jsp
@@ -14,5 +14,9 @@ Response
 <!-- This is how to read HTML form data in jsp use param."name of form field"  -->
 Welcome : ${param.studentName}
 
+<br>
+
+<!-- view page can access model attribute directly from the controller using the attribute name. in this case "message" -->
+Message: ${message}
 </body>
 </html>
--- spring-mvc-demo/src/com/luv2code/springdemo/FormController.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/FormController.java
@@ -1,6 +1,11 @@
 package com.luv2code.springdemo;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,8 +17,23 @@ public class FormController {
 		return "form-page";
 	}
 	
+//	@RequestMapping("/processForm")
+//	public String processForm() {
+//		
+//		return "form-response";
+//	}
+	
 	@RequestMapping("/processForm")
-	public String processForm() {
+	public String processForm2(HttpServletRequest request, Model model) {
+		
+		//.getParameter collects the name of the html form  field that contains the data naem
+		String name = request.getParameter("studentName");
+		
+		// convert to upperCase
+		name = "Welcome " + name.toUpperCase();
+		
+		// add result to model
+		model.addAttribute("message",name);
 		
 		return "form-response";
 	}
